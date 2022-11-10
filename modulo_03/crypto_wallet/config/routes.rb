348b9037d3861c3e136cddbb68a8e5f6e13b6e32
demershow <demersontorres520@gmail.com>
2022-11-09 21:58:54 -0300

    Aula 49: Routes and rest

--- modulo_03/crypto_wallet/config/routes.rb
+++ modulo_03/crypto_wallet/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
+
+
+  root 'welcome#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
